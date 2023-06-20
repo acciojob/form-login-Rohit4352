6cb4bd65417751362fa41146053873d4f8c9bbcd
Rohit Gupta <65344659+Rohit4352@users.noreply.github.com>
2023-06-20 09:40:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+	var first = document.querySelector('input[name=fname]').value;
+	var sec = document.querySelector('input[name=lname]').value;
+alert(first + ' ' + sec);
 }
